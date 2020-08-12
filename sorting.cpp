046bdec3bc187bfd9c4838703d89fafc976ee0a3
vharsh43 <vharsh43@gmail.com>
2020-08-12 06:04:11 +0530

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -8,9 +8,29 @@ int main() {
     cout << "Choose one to implement: \n a. heapsort \n b. mergesort \n c. quicksort \n ";
     cout << "Please input a, b, or c !!" ;
     
-    cin >> sort ;
+    cin >> sort;
+    
+    int size=0; 
+    int arr[size];
+    
+    cout << "\n Please enter the size of List" ;
+    cin >> size; 
+    
+    cout << "\n Input elements of array: \n"; 
+    
+    for(int i=0;i<size;i++){ 
+        cin >> arr[i]; 
+    } 
 
+    switch (sort)
+    {
+        case a: // heapsort
+        
+        break;
 
+        default:
+        
+    }
     
     
 
